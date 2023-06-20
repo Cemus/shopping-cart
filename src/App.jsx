4fbf39b5f536b023af990b0ce85f6dc10e223050
Cemus <24947582+Cemus@users.noreply.github.com>
2023-06-20 21:30:06 +0200

    Some work on the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,54 @@
 import { useState } from "react";
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import "./styles/App.css";
+import { Route, Routes, Link } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Collection from "./pages/Collection";
 import Cart from "./pages/Cart";
+import logo from "./assets/images/logo/logo.png";
 
 function App() {
+  const messageRemise = () => {
+    const messages = [
+      `Profitez des offres "Oulala!" et obtenez 20% de remise sur vos produits préférés !`,
+      `Du nouveau en stock !`,
+      `N'oubliez pas la crème solaire !`,
+    ];
+    const chosenMessage = messages[Math.floor(Math.random() * messages.length)];
+    return chosenMessage;
+  };
   return (
     <>
+      <header>
+        <div className="upper-header">
+          <p>{messageRemise()}</p>
+        </div>
+        <div className="mid-header">
+          <Link to="/faq">Besoin d'aide ?</Link>
+          <Link to="/">
+            <img className="logo" src={logo} />
+          </Link>
+          <Link to="/cart">Cart</Link>
+        </div>
+
+        <div className="lower-header">
+          <Link className="nav-items" to="/toys">
+            TOYS
+          </Link>
+          <Link className="nav-items" to="/clothes">
+            CLOTHES
+          </Link>
+          <Link className="nav-items" to="/equipments">
+            EQUIPMENT
+          </Link>
+          <Link className="nav-items" to="/accessories">
+            ACCESSORIES
+          </Link>
+        </div>
+      </header>
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
+        <Route path="/collection" element={<Collection />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
       </Routes>
     </>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,34 @@
+import { Link } from "react-router-dom";
+
+import "../styles/Homepage.css";
+import img1 from "../assets/images/homepage/quentinLagache.jpg";
+import img2 from "../assets/images/homepage/sheep.jpg";
+import img3 from "../assets/images/homepage/sheeps.jpg";
+import img4 from "../assets/images/homepage/sheepz.jpg";
+import img5 from "../assets/images/homepage/fun.jpg";
+
 export default function HomePage() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className="home--container">
+      <div className="home--ad-image">
+        <div className="home--text-overlay">
+          <h2>
+            Discover a curated collection of beach-inspired articles that
+            capture the essence of coastal living.
+          </h2>
+          <Link className="home--button" to="/accessories">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <div className="home--sheep-container">
+        <div className="home--sheep-pic-container">
+          <img className="home--image-sheep" src={img3} />
+          <img className="home--image-sheep" src={img2} />
+          <img className="home--image-sheep" src={img4} />
+        </div>
+        <p className="home--sheep-description">De la maroquinerie classe</p>
+      </div>
+    </div>
   );
 }
